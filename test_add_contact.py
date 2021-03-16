cd998e3c1e0df0d5054460baa1ef6cf5f4dc5055
Vlad <GrishinVl93@gmail.com>
2021-03-17 00:27:51 +0300

    Рефакторинг: действия selenium вынесены в отдельные методы Для создания контактов, логина выделены параметры в сигнатуру метода

--- test_add_contact.py
+++ test_add_contact.py
@@ -8,80 +8,91 @@ import unittest
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
         self.base_url = "https://www.google.com/"
 
-
     def test_add_contact(self):
-        driver = self.driver
-        driver.get("https://localhost/addressbook/")
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_id("LoginForm").submit()
-        driver.find_element_by_link_text("add new").click()
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("Vlad")
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys("Grishin")
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Petrov")
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys("VlGrishin")
-        driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys("Engineer")
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys("RTS COMPANY")
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("Moscow, Tverskaya street 34, h30")
-        driver.find_element_by_name("home").click()
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("+78999990073")
-        driver.find_element_by_name("mobile").click()
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("+79383434624")
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("GrishinTest@mail.ru")
-        driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("10")
-        driver.find_element_by_name("bday").click()
-        driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("October")
-        driver.find_element_by_name("bmonth").click()
-        driver.find_element_by_name("byear").click()
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("1991")
-        driver.find_element_by_name("aday").click()
-        driver.find_element_by_name("aday").click()
-        driver.find_element_by_name("address2").click()
-        driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys("Adress Test")
-        driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys("Home Test")
-        driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys("Some notes")
-        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        wd = self.wd
+        self.open_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_contact_page(wd)
+        self.create_contact(wd, "Vlad", "Grishin", "Petrov", "VlGrishin", "Engineer", "RTS COMPANY",
+                            "Moscow, Tverskaya street 34, h30", "+78999990073", "+79383434624", "GrishinTest@mail.ru",
+                            "Adress Test", "Home Test", "Some notes")
+        self.logout(wd)
+
+    def logout(self, wd):
+        # Выход из приложения
+        wd.find_element_by_link_text("Logout").click()
+
+    def create_contact(self, wd, firstname, middlename, lastname, nickname, title, companyname, addresscompany, phone,
+                       mobilephone, email, address2, additionalphone, notes):
+        # Заполняем поля и создаем контакт
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("%s" % firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("%s" % middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("%s" % lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys("%s" % nickname)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys("%s" % title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("%s" % companyname)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("%s" % addresscompany)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("%s" % phone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("%s" % mobilephone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("%s" % email)
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys("%s" % address2)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys("%s" % additionalphone)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys("%s" % notes)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def open_contact_page(self, wd):
+        # Открываем страницу создания контакта
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, name, password):
+        # Авторизация в приложении
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("%s" % name)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("%s" % password)
+        wd.find_element_by_id("LoginForm").submit()
+
+    def open_page(self, wd):
+        # Открываем стартовую страницу
+        wd.get("https://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
-            self.driver.find_element(by=how, value=what)
+            self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e:
             return False
         return True
 
     def is_alert_present(self):
         try:
-            self.driver.switch_to_alert()
+            self.wd.switch_to_alert()
         except NoAlertPresentException as e:
             return False
         return True
 
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 
 if __name__ == "__main__":
