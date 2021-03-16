b383ebea987fb0ff8e4a81ba13dfcd06d697711f
Vlad <GrishinVl93@gmail.com>
2021-03-17 00:33:22 +0300

    Рефакторинг: Выделен новый класс Contact для хранения свойств контакта

---
+++ contact.py
@@ -0,0 +1,17 @@
+
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, companyname, addresscompany, phone,
+                       mobilephone, email, address2, additionalphone, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickanme = nickname
+        self.title = title
+        self.companyname = companyname
+        self.addresscompany = addresscompany
+        self.phone = phone
+        self.mobilephone = mobilephone
+        self.email = email
+        self.address2 = address2
+        self.additionalphone = additionalphone
+        self.notes = notes
