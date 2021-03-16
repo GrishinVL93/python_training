01f14d4b234a5ebd3a741e7d9fd9466db6114fc4
Vlad <GrishinVl93@gmail.com>
2021-03-17 00:37:16 +0300

    Рефакторинг: Вместо параметров в основной тест создания контактов добавлен вызов объекта Contact (test_add_contact, create_contact)

--- test_add_contact.py
+++ test_add_contact.py
@@ -17,48 +17,47 @@ class TestAddContact(unittest.TestCase):
         self.open_page(wd)
         self.login(wd, "admin", "secret")
         self.open_contact_page(wd)
-        self.create_contact(wd, "Vlad", "Grishin", "Petrov", "VlGrishin", "Engineer", "RTS COMPANY",
+        self.create_contact(wd, Contact("Vlad", "Grishin", "Petrov", "VlGrishin", "Engineer", "RTS COMPANY",
                             "Moscow, Tverskaya street 34, h30", "+78999990073", "+79383434624", "GrishinTest@mail.ru",
-                            "Adress Test", "Home Test", "Some notes")
+                            "Adress Test", "Home Test", "Some notes"))
         self.logout(wd)
 
     def logout(self, wd):
         # Выход из приложения
         wd.find_element_by_link_text("Logout").click()
 
-    def create_contact(self, wd, firstname, middlename, lastname, nickname, title, companyname, addresscompany, phone,
-                       mobilephone, email, address2, additionalphone, notes):
+    def create_contact(self, wd, contact):
         # Заполняем поля и создаем контакт
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("%s" % firstname)
+        wd.find_element_by_name("firstname").send_keys("%s" % contact.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("%s" % middlename)
+        wd.find_element_by_name("middlename").send_keys("%s" % contact.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("%s" % lastname)
+        wd.find_element_by_name("lastname").send_keys("%s" % contact.lastname)
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("%s" % nickname)
+        wd.find_element_by_name("nickname").send_keys("%s" % contact.nickname)
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("%s" % title)
+        wd.find_element_by_name("title").send_keys("%s" % contact.title)
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("%s" % companyname)
+        wd.find_element_by_name("company").send_keys("%s" % contact.companyname)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("%s" % addresscompany)
+        wd.find_element_by_name("address").send_keys("%s" % contact.addresscompany)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("%s" % phone)
+        wd.find_element_by_name("home").send_keys("%s" % contact.phone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("%s" % mobilephone)
+        wd.find_element_by_name("mobile").send_keys("%s" % contact.mobilephone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("%s" % email)
+        wd.find_element_by_name("email").send_keys("%s" % contact.email)
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("%s" % address2)
+        wd.find_element_by_name("address2").send_keys("%s" % contact.address2)
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("%s" % additionalphone)
+        wd.find_element_by_name("phone2").send_keys("%s" % contact.additionalphone)
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("%s" % notes)
+        wd.find_element_by_name("notes").send_keys("%s" % contact.notes)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def open_contact_page(self, wd):
