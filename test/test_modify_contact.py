4abecdb4abaf86c7f3c5612999488e80d2bac4a5
Vlad <GrishinVl93@gmail.com>
2021-04-06 01:33:23 +0300

    Fix: Добавлен переход на страницу контактов для удаления\модификации контактов(если страница не открыта)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,24 +2,28 @@ from model.contact import Contact
 
 
 def test_modify_firstname(app):
+    app.contact.open_contact_page()
     if app.contact.count() == 0:
         app.contact.create(Contact("For Modify Test"))
     app.contact.modify_first_contact(Contact(firstname="New FIRSTNAME"))
 
 
 def test_modify_lastname(app):
+    app.contact.open_contact_page()
     if app.contact.count() == 0:
         app.contact.create(Contact("For Modify Test"))
     app.contact.modify_first_contact(Contact(lastname="New LASTNAME"))
 
 
 def test_modify_middlename(app):
+    app.contact.open_contact_page()
     if app.contact.count() == 0:
         app.contact.create(Contact("For Modify Test"))
     app.contact.modify_first_contact(Contact(middlename="New MIDDLENAME"))
 
 
 def test_modify_main_fields(app):
+    app.contact.open_contact_page()
     if app.contact.count() == 0:
         app.contact.create(Contact("For Modify Test"))
     app.contact.modify_first_contact(Contact("Updated", "Updated", "Updated", "Updated", "Updated", "Udated",
