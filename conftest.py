473154b4c6fe9cc306ad14a3620dfe54d098db96
Vlad <GrishinVl93@gmail.com>
2021-05-09 19:50:30 +0300

    Update: Для контактов и групп добавлены генераторы тестовых данных(json) и использование в тестах

--- conftest.py
+++ conftest.py
@@ -1,12 +1,14 @@
 import json
 import pytest
 import os
+import importlib
+import jsonpickle
 from fixture.application import Application
 from fixture.session import SessionHelper
 
-
 fixture = None
 target = None
+
 
 @pytest.fixture
 def app(request):
@@ -28,6 +30,7 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
+
     request.addfinalizer(fin)
     return fixture
 
@@ -35,3 +38,22 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
