03487b6c4fed4b4d8f7cec6f21eba772e8a87f4a
Vlad <GrishinVl93@gmail.com>
2021-05-19 02:29:16 +0300

    UPDATE: Подключение и генерация отчета Allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import pytest
+import allure
 from model.group import Group
 
 
@@ -7,11 +9,14 @@ from model.group import Group
 
 def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with allure.step("a group list"):
+        old_groups = db.get_group_list()
+    with allure.step("I add the group to the list"):
+        app.group.create(group)
+    with allure.step("the new group list is equal to the old list with the added new group"):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 # @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 # def test_add_group(app, group):
